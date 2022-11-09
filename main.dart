e8b0a3b8a3d71e4ada6f170cf3d395d8ac435197
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-11-09 18:57:08 +0300

    finished the task

--- main.dart
+++ main.dart
@@ -1,6 +1,32 @@
-void main(){
-  
-  
-  
-  
+void main() {
+  var menu = {
+    'burger': 6.5,
+    'pizza': 5,
+    'water': 1.5,
+  };
+
+  menu['cholocolateCake'] = 3;
+  menu['water'] = 0.75;
+
+  print('Menu:');
+  print('');
+  for (var element in menu.entries) {
+    print('${element.key} -> ${element.value}KD');
+  }
+
+  const order = ['pizza', 'water'];
+
+  void calculateOrder(List<String> myOrder, var menu) {
+    num total = 0;
+    for (String item in myOrder) {
+      if (menu[item] == null) {
+        return print('$item is not on the menu');
+      }
+      total += menu[item];
+    }
+    print(total);
+  }
+
+  const wrongOrder = ['rice', 'pizza'];
+  calculateOrder(wrongOrder, menu);
 }
